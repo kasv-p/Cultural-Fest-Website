929a7a030e331f965853711fc7507ce435196859
Veekshita Naidu <ballaveekshitanaidu07@gmail.com>
2023-08-19 23:44:25 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Faqs from "./components/Faqs";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Faqs />
     </div>
   );
