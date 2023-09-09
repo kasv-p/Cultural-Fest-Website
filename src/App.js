b0a872829948c3b7584e6f4e4d161acea0c6ec8d
Veekshita Naidu <ballaveekshitanaidu07@gmail.com>
2023-09-09 14:44:55 +0530

    Team info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Faqs from "./components/Faqs";
+import TeamSection from "./components/Team";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <TeamSection />
       <Faqs />
     </div>
   );
