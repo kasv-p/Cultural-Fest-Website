e1a78e1fe83b8cd95d184b3f2fb1e22e476658ed
Jaya Surya <pjayasurya2711@gmail.com>
2023-09-10 23:44:18 +0530

    added hover

--- src/components/card.js
+++ src/components/card.js
@@ -7,31 +7,32 @@ const Card = (props) => {
     title,
     date,
     month,
+    side,
     venue,
     description,
     reviewLink,
     register,
   } = props;
-
-  return (
-    <div className="card">
-      <div className="card_left">
-        <img src={imageUrl} alt={title} className="card_left_image" />
-        <div className="card_left_date">{date}</div>
-        <div className="card_left_month">{month}</div>
-      </div>
-      <div className="card_right">
-        <h1>{title}</h1>
-        <div className="card_right_details">
-          <ul>
-            <li>{venue}</li>
-          </ul>
-          <br />
+  if (side === "left") {
+    return (
+      <div className="card">
+        <div className="card_left">
+          <img src={imageUrl} alt={title} className="card_left_image" />
+          <div className="card_left_date">{date}</div>
+          <div className="card_left_month">{month}</div>
         </div>
-        <div className="card_right_description">
-          <p>{description}</p>
-        </div>
-        {/* <div className="card_right_knowmore">
+        <div className="card_right">
+          <h1>{title}</h1>
+          <div className="card_right_details">
+            <ul>
+              <li>{venue}</li>
+            </ul>
+            <br />
+          </div>
+          <div className="card_right_description">
+            <p>{description}</p>
+          </div>
+          {/* <div className="card_right_knowmore">
           <a
             href="https://youtu.be/dQw4w9WgXcQ"
             target="_blank"
@@ -40,14 +41,52 @@ const Card = (props) => {
             Know more
           </a>
         </div> */}
-        <div className="card_right_button">
-          <a href={register} target="_blank" rel="noopener noreferrer">
-            REGISTER
-          </a>
+          <div className="card_right_button">
+            <a href={register} target="_blank" rel="noopener noreferrer">
+              REGISTER
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="card">
+        <div className="card_right">
+          <h1>{title}</h1>
+          <div className="card_right_details">
+            <ul>
+              <li>{venue}</li>
+            </ul>
+            <br />
+          </div>
+          <div className="card_right_description">
+            <p>{description}</p>
+          </div>
+          {/* <div className="card_right_knowmore">
+          <a
+            href="https://youtu.be/dQw4w9WgXcQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Know more
+          </a>
+        </div> */}
+          <div className="card_right_button">
+            <a href={register} target="_blank" rel="noopener noreferrer">
+              REGISTER
+            </a>
+          </div>
+        </div>
+        <div className="card_left">
+          <img src={imageUrl} alt={title} className="card_left_image" />
+          <div className="card_left_time">
+            <div className="card_left_date">{date}</div>
+            <div className="card_left_month">{month}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
-
 export default Card;
--- src/components/events.js
+++ src/components/events.js
@@ -5,15 +5,17 @@ const Events = () => {
       <Card
         imageUrl="https://s3-us-west-2.amazonaws.com/s.cdpn.io/343086/h8fnwL1.png"
         title="KILL BILL"
+        side={"left"}
         date="24"
         month="AUGUST"
         venue="The Grand Illusion Cinema"
-        description={`The lead character, called 'The Bride,' was a me The lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was ater, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character`} // Add the full review here
+        description={`The The Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion CinemaThe Grand Illusion Cinemalead character, called 'The Bride,' was a me The lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was ater, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character, called 'The Bride,' was a membeThe lead character`} // Add the full review here
         register="http://www.imdb.com/title/tt0266697/plotsummary?ref_=tt_stry_pl"
       />
       <Card
         imageUrl="https://s3-us-west-2.amazonaws.com/s.cdpn.io/343086/h8fnwL1.png"
         title="KILL BILL 1"
+        side={"right"}
         date="24"
         month="AUGUST"
         venue="The Grand Illusion Cinema"
@@ -23,6 +25,7 @@ const Events = () => {
       <Card
         imageUrl="https://s3-us-west-2.amazonaws.com/s.cdpn.io/343086/h8fnwL1.png"
         title="KILL BILL 2"
+        side={"left"}
         date="24"
         month="AUGUST"
         venue="The Grand Illusion Cinema"
