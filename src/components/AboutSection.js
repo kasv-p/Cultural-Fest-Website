4f7966f5e00fb326f68a28e7bd45cc4c1a68ff0b
kasv-p <ksvd1234@gmail.com>
2023-09-24 12:28:55 +0530

    added content for about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -26,24 +26,8 @@ function AboutSection() {
                 </div>
                 <div className="description">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                    </p>
+
+                        IIT Dharwad is proud to announce our inaugural event, a prelude to our Annual Cultural Festival, <b>AVIRAL: 2023</b>, marking a significant milestone in our cultural journey. As we embark on this remarkable venture, we aspire to create an enduring legacy within the realms of art and culture. This event is a fusion of diverse art forms, a celebration of creativity that bridges local and global cultures. With participants from all corners of the nation, the event cultivates a competitive spirit, motivating and recognizing the exceptional talents among students. We offer a stage for artists to display their craft, encouraging healthy competition and providing a glimpse of outstanding performances that can serve as inspiration for aspiring talents. We extend an open invitation to all, inviting you to unite with us and revel in the splendor of art and culture, the IIT Dharwad way. Join us on this inaugural occasion, embracing the rich tapestry of human creativity.                    </p>
                 </div>
             </header>
         </div>
